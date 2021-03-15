9bd93663ccc0690672f7100a220be6d35a2bb8e3
Kyle Sherrington <kylejsherrington@gmail.com>
2021-03-15 12:06:42 -0700

    Initial commit

---
+++ Guidebook-App/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Guidebook-App
+//
+//  Created by Kyle Sherrington on 2021-03-15.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var INFO_VIEWCONTROLLER = "InfoVC"
+    static var MAP_VIEWCONTROLLER = "MapVC"
+    static var NOTES_VIEWCONTROLLER = "NotesVC"
+    
+}
+
---
+++ Guidebook-App/ViewControllers/PlaceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlaceViewController.swift
+//  Guidebook-App
+//
+//  Created by Kyle Sherrington on 2021-03-15.
+//
+
+import UIKit
+
+class PlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
